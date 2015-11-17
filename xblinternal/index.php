b8003b2d78514bf76f33c12d74b5f1b553b9e03d
David Fu <ydavidf@gmail.com>
2015-11-17 09:59:15 -0800

    Inital commit of internal site

---
+++ xblinternal/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> XBL Internal </title>
+</head>
+<body>
+
+<h1>XBL Internal Site</h1>
+
+<p>Links</p>
+<a href="downloads.php">Downloads</a>
+
+</body>
+</html>
