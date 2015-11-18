a7768e99d8fa937da3f78d67ffad0ef5c518c117
David Fu <ydavidf@gmail.com>
2015-11-17 17:22:36 -0800

    Basic login and dummy home screens have been added. Requires debugging.

---
+++ xblinternal/login.php
@@ -0,0 +1,35 @@
+<?php
+//basic init block
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "server";
+$connection = mysqli_connect(server, user, pass, db);
+
+//Error Handling block
+if(mysqli_connect_errno)
+{
+	echo mysqli_connect_error(). "Failed to connect to the database. Please contact the server administrator.";
+}
+
+//User login block
+if(isset($_POST['login']))
+{
+	$email = mysqli_real_escape_string($connection, $_POST['email']);
+	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	$select_user_query = "select * from where user_email = '$email' AND user_password = '$password'";
+	$run_user_query = mysqli_query($connection, $select_user_query);
+	$return_user = mysqli_num_rows($run_user_query);
+	
+	//User validation
+	if($return_user > 0)
+	{
+		$_SESSION['user_email'] = $email;
+		echo "<script>window.open('home.php','_self')</script>";
+	}
+	else
+	{
+		echo "<script>alert('Email or password is incorrect')</script>";
+	}
+}
+?>
