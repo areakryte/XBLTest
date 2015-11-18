b04b9f8363cd28ba422c320f5e00f6faff38a187
David Fu <ydavidf@gmail.com>
2015-11-17 16:21:13 -0800

    Cleaned up archaic code.Implementing basic database tests.

--- xblinternal/index.php
+++ xblinternal/index.php
@@ -1,5 +1,5 @@
 <?php 
-// Start a session
+//Start a session
 //session_start();
 //require_once ('db_connect.inc.php');
 //require_once ("functions.inc.php");
@@ -16,9 +16,22 @@
 
 <p>Links</p>
 <a href="downloads.php">Downloads</a>
+<br>
 
 <?php
 //include "login.php";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$info = "test";
+
+$conn = new mysqli($servername, $username, $password, $info);
+if($conn->connect_error)
+{
+	die("Connection Failed: " . $conn->connect_error);
+}
+echo "Success";
+$conn->close();
 ?>
 
 </body>
